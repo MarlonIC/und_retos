db6360584d5ab73b14ffc33d0f258f868842d766
MarlonIc <marlonricslim@gmail.com>
2021-03-19 14:54:12 -0500

    feat(challenge): add resources

--- javascript/reto3.js
+++ javascript/reto3.js
@@ -1,3 +1,27 @@
+/**
+ Érase una vez, en un camino a través del viejo oeste montañoso salvaje
+ A un hombre se le dieron instrucciones para ir de un punto a otro. Las direcciones eran "NORTE", "SUR", "OESTE", "ESTE". Claramente, "NORTE" y "SUR" son opuestos, "OESTE" y "ESTE" también.
+ Ir en una dirección y regresar en la dirección opuesta de inmediato es un esfuerzo innecesario. Dado que este es el salvaje oeste, con un clima espantoso y poca agua, es importante que se ahorre algo de energía, de lo contrario, podría morir de sed.
+ Cómo cruzar un desierto montañoso de manera inteligente. Las instrucciones dadas al hombre son, por ejemplo, las siguientes (según el idioma):
+
+ ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+ or
+ { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+ or
+ [North, South, South, East, West, North, West]
+
+ Inmediatamente puede ver que ir "NORTE" e inmediatamente "SUR" no es razonable, ¡mejor quédese en el mismo lugar! Entonces, la tarea es darle al hombre una versión simplificada del plan. Un mejor plan en este caso es simplemente:
+
+ ["WEST"]
+ or
+ { "WEST" }
+ or
+ [West]
+
+ Reto:
+ Escriba una función dirReduc que tomará una matriz de cadenas y devolverá una matriz de cadenas con las direcciones innecesarias eliminadas (W <-> E o S <-> N una al lado de la otra).
+ */
+
 function dirReduction(directions) {
     const filterValidate = directions.filter((direction) => {
         if (typeof direction !== 'string' || !['north', 'south', 'east', 'west'].includes(direction.toLowerCase())) {
@@ -50,6 +74,6 @@ function dirReduction(directions) {
     return directionOptimize;
 }
 
-const matrix = ['North', 'South', 'South', 'East', 'West', 'North', 'West'];
+const matrix = ["NORTH", "WEST", "SOUTH", "EAST"];
 const result = dirReduction(matrix);
 console.log(result);
---
+++ javascript/reto4.js
@@ -0,0 +1,38 @@
+/**
+ Cree una clase RomanNumerals que pueda convertir un número romano en un valor entero.
+ Los números romanos modernos se escriben expresando cada dígito por separado comenzando con el dígito más a la izquierda y saltando cualquier dígito con un valor de cero.
+
+ En números romanos se representa 1990:
+   - 1000 = M
+   - 900 = CM
+   - 90 = XC
+   - resultando en MCMXC.
+ 2008 se escribe como:
+   - 2000 = MM
+   - 8 = VIII
+   - resultado MMVIII.
+ 1666 usa cada símbolo romano en orden descendente: MDCLXVI.
+
+ Equivalencias de números romanos
+   - I -> 1
+   - V -> 5
+   - X -> 10
+   - L -> 50
+   - C -> 100
+   - D -> 500
+   - M -> 1000
+
+ Ejemplo codigo:
+
+   const RomanNumerals = {
+    toRoman: () => {},
+    fromRoman: () => {}
+   }
+
+   RomanNumerals.toRoman(1000); // should return 'M'
+   RomanNumerals.fromRoman('M'); // should return 1000
+*/
+
+class Reto4 {
+    
+}
