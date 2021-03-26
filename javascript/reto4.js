a7d7bf8885f217df8564d1883455345a7c2927c9
MarlonIc <marlonricslim@gmail.com>
2021-03-26 15:52:42 -0500

    feat(challenge): convert number to roman

--- javascript/reto4.js
+++ javascript/reto4.js
@@ -33,6 +33,53 @@
    RomanNumerals.fromRoman('M'); // should return 1000
 */
 
-class Reto4 {
-    
+const RomanNumerals = {
+  toRoman: (number) => {
+    const romans = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1};
+    const lengthNumber = number.toString().length;
+    let valueRoman = '';
+
+    for (let i=0; i < lengthNumber; i++) {
+      const numberComplete = number % parseInt('1'  + '0'.repeat(lengthNumber - i));
+      const numberUnit = Math.trunc(numberComplete / parseInt('1'  + '0'.repeat(lengthNumber - (i+1))));
+      const numberVolume = numberUnit + '0'.repeat(lengthNumber-i-1);
+
+      for (const romansKey in romans) {
+        if (romans[romansKey] <= numberVolume) {
+          if (romans[romansKey] === parseInt(numberVolume)) {
+            valueRoman += romansKey;
+            break;
+          }
+          if (numberUnit === 9) {
+            const position_one = parseInt(1 + '0'.repeat(lengthNumber - i - 1));
+            const position_two = parseInt(10 + '0'.repeat(lengthNumber - i - 1));
+            valueRoman += Object.keys(romans).find(key => romans[key] === position_one) + Object.keys(romans).find(key => romans[key] === position_two);
+            break;
+          }
+          if (numberUnit >= 5) {
+            const position_one = parseInt(5 + '0'.repeat(lengthNumber - i - 1));
+            const position_two = parseInt(1 + '0'.repeat(lengthNumber - i - 1));
+            valueRoman += Object.keys(romans).find(key => romans[key] === position_one) + Object.keys(romans).find(key => romans[key] === position_two).repeat(numberUnit-5);
+            break;
+          }
+          if (numberUnit === 4) {
+            const position = parseInt(5 + '0'.repeat(lengthNumber - i - 1));
+            valueRoman += romansKey + Object.keys(romans).find(key => romans[key] === position);
+            break;
+          }
+          valueRoman += romansKey.repeat(numberUnit);
+          break;
+        }
+      }
+    }
+    return valueRoman;
+  },
+  fromRoman: (roman) => {
+
+  }
 }
+
+const number = 2008;
+const to_roman = RomanNumerals.toRoman(number);
+const to_number = RomanNumerals.fromRoman('M');
+console.log('Número romano de', number, 'es:', to_roman);
